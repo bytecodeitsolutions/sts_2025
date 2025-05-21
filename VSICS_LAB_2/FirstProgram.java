f21b8535016a3127821e8c834329c4d275e96159
Kayum1234 <kayumansari92@gmail.com>
2025-05-21 15:56:07 +0530

    first program
    command to compile and run

---
+++ VSICS_LAB_2/FirstProgram.java
@@ -0,0 +1,13 @@
+class FirstProgram 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	}
+}
+
+>javac FirstProgram.java
+
+>java FirstProgram
+
+Hello World!
---
+++ VSICS_LAB_2/installation_links.java
@@ -0,0 +1,28 @@
+/* 
+  Java:
+  -----
+  
+  Modules of java:
+  
+  JSE
+  JEE
+  JME
+  
+  JSE
+  ----
+  
+  java standard edition
+  java 2 platform standard edition
+  
+  we can download JSE in the form of JDK.
+  
+  standalone app
+  
+  1. Download JDK
+  
+  https://www.openlogic.com/openjdk-downloads
+  
+  2. Any Editor Software like Editplus , Notepad , Notepad++,.....
+  https://www.editplus.com/download.html
+  
+*/
