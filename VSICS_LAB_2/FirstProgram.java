c51cb8ca14a3f84bf2191c23089f2fca7941ed35
Kayum1234 <kayumansari92@gmail.com>
2025-05-21 17:57:13 +0530

    updated
    updated

--- VSICS_LAB_2/FirstProgram.java
+++ VSICS_LAB_2/FirstProgram.java
@@ -1,4 +1,22 @@
-class FirstProgram 
+/*
+   To Run a Java Program:
+   ---------------------
+   
+   Dowload Editplus
+      |-https://www.editplus.com/download.html
+   
+   Dowload JDK
+      |-https://www.openlogic.com/openjdk-downloads
+	  
+	  Once these software will downloaded and installed 
+	  
+	  Open Editplus
+	    |-Select a directory to store java program
+		
+		Then to write the java program
+		   |-File -->new --->java
+		   
+class Test 
 {
 	public static void main(String[] args) 
 	{
@@ -6,8 +24,38 @@ class FirstProgram
 	}
 }
 
->javac FirstProgram.java
+    Save the java program
+	ClassName.java
+	FirstProgram.java
+	
+	---------------------------
+	
+	Compile the java program
+	   |-open command prompt
+	
+	
+	To compile:
+	
+	javac    FileName.java
+	
+	D:\java_programs>javac FirstProgram.java
 
->java FirstProgram
-
-Hello World!
+    D:\java_programs>java FirstProgram
+	
+	To run the java program
+	========================
+	
+	java ClassName
+	java  FirstProgram
+	
+	Output
+    Hello World!
+     
+*/
+class FirstProgram 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	}
+}
