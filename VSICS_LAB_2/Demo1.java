e87f56cd13e416beefeff9505d89ab30266457cd
Kayum1234 <kayumansari92@gmail.com>
2025-05-21 17:55:20 +0530

    how to compile and run
    all syntax

--- VSICS_LAB_2/Demo1.java
+++ VSICS_LAB_2/Demo1.java
@@ -9,18 +9,7 @@ class  Dog
 class Demo1
 {
 	public static void main(String[] args){
-		Dog  d1=new Dog();
-		Dog  d2=new Dog();
-		Dog  d3=new Dog();
-		
-		d1.bark();
-		d2.bark();
-		d3.bark();
-		
-		System.out.println("==================");
-		d1.bark();
-		d1.bark();
-		d1.bark();
+	System.out.println(7346+3746);
 	}
 }
 
---
+++ java_backup/FirstProgram.java
@@ -0,0 +1,61 @@
+/*
+   To Run a Java Program:
+   ---------------------
+   
+   Dowload Editplus
+      |-https://www.editplus.com/download.html
+   
+   Dowload JDK
+      |-https://www.openlogic.com/openjdk-downloads
+	  
+	  Once these software will downloaded and installed 
+	  
+	  Open Editplus
+	    |-Select a directory to store java program
+		
+		Then to write the java program
+		   |-File -->new --->java
+		   
+class Test 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	}
+}
+
+    Save the java program
+	ClassName.java
+	FirstProgram.java
+	
+	---------------------------
+	
+	Compile the java program
+	   |-open command prompt
+	
+	
+	To compile:
+	
+	javac    FileName.java
+	
+	D:\java_programs>javac FirstProgram.java
+
+    D:\java_programs>java FirstProgram
+	
+	To run the java program
+	========================
+	
+	java ClassName
+	java  FirstProgram
+	
+	Output
+    Hello World!
+     
+*/
+class FirstProgram 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	}
+}
